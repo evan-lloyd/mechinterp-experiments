fix attention mask, use dataloader in tinystories

tiny_stories_sae/tiny_stories_sae/activation_cache.py
@@ -14,7 +14,7 @@ from transformers import AutoTokenizer
 
 from .data_batch import DataBatch
 from .ops import ensure_directory
-from .tokenization import CONTEXT_LENGTH, input_generator
+from .tokenization import DEFAULT_CONTEXT_LENGTH, make_dataloader
 
 
 @torch.no_grad
@@ -147,13 +147,13 @@ def build_cache(
         model.config.num_layers,
         inference_batch_size,
         1,
-        CONTEXT_LENGTH,
+        DEFAULT_CONTEXT_LENGTH,
         model.config.hidden_size,
     )
     num_used_tokens = 0
 
     model.eval()
-    for batch in input_generator(
+    for batch in make_dataloader(
         model,
         tokenizer,
         dataset,

tiny_stories_sae/tiny_stories_sae/training_step/training_step.py
@@ -6,7 +6,7 @@ import torch
 from ..activation_data import ActivationBatch, TrainingBatch, make_activation_batch
 from ..data_batch import DataBatch
 from ..ops import clone_sae
-from ..replacement_model import ReplacementModel
+from ..replacement_model import ReplacementModel, make_replacement_model
 from ..sae import SAE
 
 if TYPE_CHECKING:
@@ -22,7 +22,7 @@ class Stepper(ABC):
         base_model: torch.nn.Module,
         replacement_model: torch.nn.Module,
     ):
-        self.base_model = base_model
+        self.base_model = make_replacement_model(base_model, {})
         self.replacement_model = replacement_model
 
     @property